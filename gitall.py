25c609d7a4ff99570e611d6d8f9647725723b7b3
xerobia <bob.xy.chen@gmail.com>
2017-04-20 23:29:06 +1000

    ['yolo', 'spaces']

--- gitall.py
+++ gitall.py
@@ -9,18 +9,19 @@ from subprocess import *
 
 parser = argparse.ArgumentParser()
 parser.add_argument("Version_Notes", help="the message/notes for this release",
-                    nargs="?", default = "Automated Message")
+                    nargs="+", default = "Automated Message")
 
 args = parser.parse_args()
 
 print args.Version_Notes
 
-
-
-
 try:
     check_output('git commit -a -m "{}"'.format(args.Version_Notes), shell = True, stderr=STDOUT)
+    print "Local Commit Finished with message: " + args.Version_Notes
+    print "Begining Push to server"
     check_output('git push', shell = True, stderr=STDOUT)
+    print "Finished Push"
+
 except CalledProcessError as error:
     print "failed"
     print "this was teh error message: "
