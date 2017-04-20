488a590e0ec581702e80511c1b0700b0f2e69eba
xerobia <bob.xy.chen@gmail.com>
2017-04-20 23:22:35 +1000

    KK

---
+++ gitall.py
@@ -0,0 +1,29 @@
+#gitall
+
+#commits ALL files, pushes all to cloud
+
+#does commit -a 
+#does git push
+import argparse
+from subprocess import *
+
+parser = argparse.ArgumentParser()
+parser.add_argument("Version_Notes", help="the message/notes for this release",
+                    nargs="?", default = "Automated Message")
+
+args = parser.parse_args()
+
+print args.Version_Notes
+
+
+
+
+try:
+    check_output('git commit -a -m "{}"'.format(args.Version_Notes), shell = True, stderr=STDOUT)
+    check_output('git push', shell = True, stderr=STDOUT)
+except CalledProcessError as error:
+    print "failed"
+    print "this was teh error message: "
+    print error.output
+    print ""
+    print "please ensure that you are running this from the dir that contains the appropriate git"
